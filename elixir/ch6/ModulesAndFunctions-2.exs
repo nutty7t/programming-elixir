9f25b45557a7382617b979b0235f301aed458874
ナッティ <hello@nutty.cc>
2018-12-08 22:32:14 -0700

    Complete ModulesAndFunctions-2 exercise

---
+++ elixir/ch6/ModulesAndFunctions-2.exs
@@ -0,0 +1,12 @@
+# Exercise: ModulesAndFunctions-2
+# Chapter 6, Page 55
+
+# $ iex "ModulesAndFunctions-1.exs"
+# iex(1)> Times.triple 3
+# 9
+
+# $ iex
+# iex(1)> c "ModulesAndFunctions-1.exs"
+# [Times]
+# iex(2)> Times.triple 4
+# 12
