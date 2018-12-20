228f91c44f34b27287f11fdd7f6f307770220fff
ナッティ <hello@nutty.cc>
2018-12-19 20:29:43 -0700

    Complete StringsAndBinaries-3 exercise

---
+++ elixir/ch11/StringsAndBinaries-3.exs
@@ -0,0 +1,10 @@
+# Exercise: StringsAndBinaries-3
+# Chapter 11, Page 123
+
+# Run this in the IEx.
+[ 'cat' | 'dog' ]
+
+# IEx prints 'cat' as a string, but 'dog' as individual
+# letters because 'cat' is an embedded list. Lists are
+# unprintable, so the characters 'd', 'o', and 'g' are
+# outputted as integers.
